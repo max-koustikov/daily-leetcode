03bfd28512e5337d46024232e8f6b5af94a778e9
Max Koustikov <87721818+max-koustikov@users.noreply.github.com>
2023-12-25 20:40:19 -0500

    Update send_mail.py

--- .github/workflows/send_mail.py
+++ .github/workflows/send_mail.py
@@ -1,1 +1,33 @@
+import smtplib
+from email.mime.text import MIMEText
+from datetime import datetime, timedelta
+
+# Email Credentials and Content (will be provided as secrets in GitHub Actions)
+sender_email = "your_email@gmail.com"  # Replace with your email address
+password = "your_app_password"  # Replace with your app password
+recipient_email = "recipient_email@example.com"  # Replace with recipient email
+subject = "Automated Email from Python"
+body = "This is a test email sent at 8:30 PM."
+
+# Get current time and calculate target time
+now = datetime.now()
+target_time = now.replace(hour=20, minute=30)
+
+# Check if it's the designated time to send the email
+if target_time - now <= timedelta(seconds=60):
+
+    # Create the message
+    message = MIMEText(body)
+    message['Subject'] = subject
+    message['From'] = sender_email
+    message['To'] = recipient_email
+
+    # Establish a secure connection with Gmail's SMTP server
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, recipient_email, message.as_string())
+
+    print("Email sent successfully at 8:30 PM!")
+else:
+    print("Not the designated time to send the email. Current time:", now)
 
